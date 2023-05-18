dec2c8edcb2949648b6e7c762d7384d8307e8257
Guillermo Valarezo Guzman <gvalarezog@gmail.com>
2023-05-17 22:21:34 -0500

    ADD encapsulamiento

--- persona.py
+++ persona.py
@@ -7,7 +7,8 @@ class Persona:
     '''
     Constructor o metodo que inicializa una objeto
     '''
-    def __init__(self, cedula=None, email=None, nombre=None, apellido=None, genero=None, ocupacion=None ):
+    def __init__(self, cedula:str='', email:str=None, nombre:str=None, apellido:str=None
+                 , genero:str=None, ocupacion:str=None):
         self._nombre = nombre
         self._apellido = apellido
         self._cedula = cedula
@@ -16,36 +17,102 @@ class Persona:
         self._ocupacion = ocupacion
 
 
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, nombre_cambiado):
+        self._nombre = nombre_cambiado
+
+    @property
+    def apellido(self):
+        return self._apellido
+
+    @apellido.setter
+    def apellido(self, apellido):
+        self._apellido = apellido
+
+    @property
+    def cedula(self):
+        return self._cedula
+
+    @cedula.setter
+    def cedula(self, cedula):
+        if len(cedula) == 10:
+            self._cedula = cedula
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, email):
+        self._email = email
+
+    @property
+    def ocupacion(self):
+        return self._ocupacion
+
+    @ocupacion.setter
+    def ocupacion(self, ocupacion):
+        self._ocupacion = ocupacion
+
+    @property
+    def genero(self):
+        return self._genero
+
+    @genero.setter
+    def genero(self, genero):
+        self._genero = genero
+
     '''
     Redefinición del metodo STR
     '''
     def __str__(self):
         # return f'Persona [nombre: {self._nombre}, apellido: {self._apellido}, email: {self._email}]'
         return f'Persona: {self.__dict__.__str__()}'
+    '''
+    Display name
+    '''
+    def nombre_mostrar(self, mostrar_correo):
+        if mostrar_correo:
+            print(f'{self._nombre} {self._apellido}, emial: {self._email}')
+        else:
+            print(f'{self._nombre} {self._apellido}')
+
+    def obtener_nombre_mostrar(self):
+        return f'{self._nombre} {self._apellido}'
 
 
 
 if __name__ == '__main__':
-    objetoPersona1 = Persona(nombre='Luis', genero='Masculino', ocupacion='Estudiante', apellido='Perez', email='lp@mail.com')
-    # print(f'Nombre: {objetoPersona1._nombre}')
-    # print(f'Apellido: {objetoPersona1._apellido}')
-    # print(f'Genero: {objetoPersona1._genero}')
-    # print(f'Ocupación: {objetoPersona1._ocupacion}')
-    # print(f'Email: {objetoPersona1._email}')
+    objetoPersona1 = Persona(nombre='Luis', genero='Masculino', ocupacion='Estudiante', apellido='Perez',
+                             email='lp@mail.com')
     print(objetoPersona1.__str__())
-    # print('*'.center(__width=50,__fillchar='*'))
-    objetoPersona2 = Persona(genero='Femenino', ocupacion='Doctor', nombre='Maria', apellido='Paz', email='mp@mail.com')
-    # print(f'Nombre: {objetoPersona2._nombre}')
-    # print(f'Apellido: {objetoPersona2._apellido}')
-    # print(f'Genero: {objetoPersona2._genero}')
-    # print(f'Ocupación: {objetoPersona2._ocupacion}')
-    # print(f'Email: {objetoPersona2._email}')
-    print(objetoPersona2)
 
-    objPersona3 =  Persona()
-    # print(f'Nombre: {objPersona3._nombre}')
-    # print(f'Apellido: {objPersona3._apellido}')
-    # print(f'Genero: {objPersona3._genero}')
-    # print(f'Ocupación: {objPersona3._ocupacion}')
-    # print(f'Email: {objPersona3._email}')
-    print(objPersona3)
+
+    objetoPersona1._nombre = 'Carlos' # no es correcto
+    objetoPersona1._email = 'cp@mail.com' # no es correcto
+    print(objetoPersona1._nombre) # no es correcto
+
+
+    objetoPersona1.nombre = 'Felipe' # si es correcto
+    print(objetoPersona1.nombre) # si es correcto
+    print(objetoPersona1.apellido)
+    objetoPersona1.email = 'fp@mail.com'
+    print(objetoPersona1.email)
+
+
+    print(objetoPersona1.__str__())
+    print('*'.center(100,'*'))
+
+
+    objP2 = Persona(cedula='0123456799')
+    print(objP2)
+
+    objP3 = Persona(cedula=123456799)
+    print(objP3)
+    objP3.cedula = '0123456789'
+    print(objP3)
+
